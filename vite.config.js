4bf47bfba0ebc74b4bfb3a5f201f7cac127816af
Aishee Dutta <125751164+Adn644@users.noreply.github.com>
2026-05-29 02:08:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default defineConfig({
   plugins: [react()],
